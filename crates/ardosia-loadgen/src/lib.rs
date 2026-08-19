f9638cad761b0ee191afcc8957da47fcf8d6e3d5
Kayllon Douglas Teixeira Almeida <76278156+Millesant@users.noreply.github.com>
2026-08-18 22:25:58 -0300

    feat: export profiling modules

--- crates/ardosia-loadgen/src/lib.rs
+++ crates/ardosia-loadgen/src/lib.rs
@@ -3,9 +3,11 @@ mod sampling;
 mod server_target;
 
 pub mod child_protocol;
+pub mod cli;
 pub mod environment;
 pub mod frame;
 pub mod latency;
+pub mod profiling;
 pub mod report;
 pub mod resource;
 pub mod runner;
